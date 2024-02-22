717cb028ee8dac19b909ceeadee5f3187d372a09
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-02-22 13:15:29 -0500

    fix: youtube search
    Introduce query to find music more precisely
    
    Remove previous search api request template to more refined one.
    
    BREAKING CHANGE: 'by' in access_youtube_music is now used to make searching for music much accurate

--- Youtube/access_youtube_music.py
+++ Youtube/access_youtube_music.py
@@ -3,13 +3,29 @@ from ytmusicapi import YTMusic
 ytmusic = YTMusic("oauth.json")
 
 playlistId = ytmusic.create_playlist("test", "test description")
-# search_results = ytmusic.search("Fortnite Raise Up Lobby Music")
-search_results = [{'videoId': '6io0g7WuHaI'}]
-ytmusic.add_playlist_items(playlistId, [search_results[0]['videoId']])
 
-print('Done')
+search_results = ytmusic.search("car radio by twenty one pilots", "songs")
 
+# ytmusic.add_playlist_items(playlistId, [search_results[0]['videoId']])
 
-# When I search for a music on YouTube music it shows id after watch ,
-# that is what I will use, I just need to find it in the list that it give me
+# Extract relevant information from the search results
+songs_info = [{'title': song['title'], 'videoId': song['videoId']} for song in search_results]
 
+# Find the text before "by" in the search query
+# Used "by" to improve youtube music search as it was giving remix of the songs before
+query = ""
+index_of_by = query.find(" by ")
+search_text = query[:index_of_by] if index_of_by != -1 else query
+
+# Find the corresponding video ID
+video_id = None
+for song in songs_info:
+    if search_text in song['title']:
+        video_id = song['videoId']
+        break
+
+if video_id:
+    ytmusic.add_playlist_items(playlistId, [video_id])
+    print("Song added to playlist")
+else:
+    print("Song not found")
