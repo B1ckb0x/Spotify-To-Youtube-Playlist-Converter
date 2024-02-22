8c67806283a723fccb9e63eb3909bb1894ec56e8
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-02-22 16:05:27 -0500

    feat: Manual Music upload
    Using track_names.txt from Spotify to get the names of the music and their artist and upload to the playlist we created.

--- Youtube/access_youtube_music.py
+++ Youtube/access_youtube_music.py
@@ -4,28 +4,30 @@ ytmusic = YTMusic("oauth.json")
 
 playlistId = ytmusic.create_playlist("test", "test description")
 
-search_results = ytmusic.search("car radio by twenty one pilots", "songs")
+with open("../Spotify/track_artist_pairs.txt") as track:
+    for item in track:
+        search_results = ytmusic.search(item, "songs")
 
-# ytmusic.add_playlist_items(playlistId, [search_results[0]['videoId']])
+        # Extract relevant information from the search results
+        songs_info = [{'title': song['title'], 'videoId': song['videoId']} for song in search_results]
 
-# Extract relevant information from the search results
-songs_info = [{'title': song['title'], 'videoId': song['videoId']} for song in search_results]
+        # Find the text before "by" in the search query
+        # Used "by" to improve youtube music search as it was giving remix of the songs before
+        query = ""
+        index_of_by = query.find(" by ")
+        search_text = query[:index_of_by] if index_of_by != -1 else query
 
-# Find the text before "by" in the search query
-# Used "by" to improve youtube music search as it was giving remix of the songs before
-query = ""
-index_of_by = query.find(" by ")
-search_text = query[:index_of_by] if index_of_by != -1 else query
+        # Find the corresponding video ID
+        video_id = None
+        for song in songs_info:
+            if search_text in song['title']:
+                video_id = song['videoId']
+                break
 
-# Find the corresponding video ID
-video_id = None
-for song in songs_info:
-    if search_text in song['title']:
-        video_id = song['videoId']
-        break
+        if video_id:
+            ytmusic.add_playlist_items(playlistId, [video_id])
+            print("Song added to playlist")
+        else:
+            print("Song not found")
 
-if video_id:
-    ytmusic.add_playlist_items(playlistId, [video_id])
-    print("Song added to playlist")
-else:
-    print("Song not found")
+print("Completed")
