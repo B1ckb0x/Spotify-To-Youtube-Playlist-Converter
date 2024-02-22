89f2e47aea9b62170018995ce66258219ca398fd
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-02-22 16:03:26 -0500

    feat: Get track and artist name
    Modify code to get track and artist name from spotify api, then concatenate both track and artist name with by in between for our search accuracy.
    
    This then writes to the track_artist_pairs.txt file after concatenation for later use

--- Spotify/get_spotify_playlist.py
+++ Spotify/get_spotify_playlist.py
@@ -2,12 +2,9 @@ import requests
 
 from Spotify.get_api import api_needs_refresh
 
-# file = "API/api.txt"
-
 access_token = api_needs_refresh()
-# print(access_token)
 
-url = 'https://api.spotify.com/v1/playlists/36qx4wzHmjl8BRVWxDg4ph/tracks?fields=items%28track%28name%29%29'
+url = 'https://api.spotify.com/v1/playlists/36qx4wzHmjl8BRVWxDg4ph/tracks?fields=items%28track%28name%2Cartists%28name%29%29%29'
 
 headers = {
     'Authorization': f'Bearer {access_token}'
@@ -16,18 +13,11 @@ headers = {
 response = requests.get(url, headers=headers)
 json_data = response.json()
 
-track_names = [item['track']['name'] for item in json_data['items']]
-print("Track Names: ", track_names)
+# Extract track names and artist names and concatenate them
+track_artist_pairs = [f"{item['track']['name']} by {item['track']['artists'][0]['name']}" for item in
+                      json_data['items']]
 
-with open('track_names.txt', 'w', encoding='utf-8') as file:
-    for name in track_names:
-        file.write(name + "\n")
-
-# json_format = json.dumps(json_data, indent=2)
-# html_content = f"<pre>{json_format}</pre>"
-# soup = BeautifulSoup(html_content, 'html.parser')
-#
-# with open('output.html', 'w', encoding='utf-8') as file:
-#     file.write(str(soup))
-#
-# webbrowser.open('output.html')
+# Write track names and artist names to a file
+with open('track_artist_pairs.txt', 'w', encoding='utf-8') as file:
+    for pair in track_artist_pairs:
+        file.write(f"{pair}\n")
